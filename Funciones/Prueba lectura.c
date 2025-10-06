2524c597da2eba7cadb072191425fe3576386967
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2025-10-06 20:53:18 -0300

    Funcion prueba lectura actualizada

--- Funciones/Prueba lectura.c
+++ Funciones/Prueba lectura.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  char linea[20000];
-  FILE *archivo;
-  printf("Abriendo archivo para lectura\n");
-  archivo = fopen("2019_Matricula.csv", "r");
-  if (archivo != NULL) {
-    int n = 1;
-    while (fgets(linea, 20000, archivo)) {
-      printf("%d: %s", n++, linea);
+int main(){
+    char linea[20000];
+    FILE *archivo;
+    printf("Abriendo archivo para lectura\n");
+    archivo = fopen("2019_Matricula.csv", "r");
+    if(archivo != NULL){
+        while(fgets(linea, 20000, archivo) != NULL){
+            printf("%s", linea);
+        }
+        fclose(archivo);
+    }else{
+        printf("\nNo se ha podido abrir el archivo\n");
     }
-
-    fclose(archivo);
-  } else {
-    printf("\nNo se ha podido abrir el archivo\n");
-  }
-  return 0;
+    return 0;
 }
