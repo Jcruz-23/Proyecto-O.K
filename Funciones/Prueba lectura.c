1b713370cda4e3b512a7190cbcdd8c233e03d0f5
Jcruz-23 <Jsanchez041@alumnos.iua.edu.com>
2025-10-07 09:21:53 -0300

    Actualizacion de rama

--- Funciones/Prueba lectura.c
+++ Funciones/Prueba lectura.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 int main(){
-    char linea[200];
+    char linea[200000];
     FILE *archivo;
     printf("Abriendo archivo para lectura\n");
     archivo = fopen("2019_Matricula.csv", "r");
@@ -15,5 +17,6 @@ int main(){
     }else{
         printf("\nNo se ha podido abrir el archivo\n");
     }
-    return 0;
 }
+
+
