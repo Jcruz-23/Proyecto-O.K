39c1a0eebef0c7704211a4d693eb06840fe355f4
mmartinn02 <mmartinn02@gmail.com>
2025-10-06 22:55:50 -0300

    prototipos actualizados

--- Cabeceras/Prototipos.h
+++ Cabeceras/Prototipos.h
@@ -1,7 +1,7 @@
 #ifndef PROTOTIPOS_H
 #define PROTOTIPOS_H
 
-//********ACLARACION , SE DEBERA MANEJAR CON PILA ES DECIR MEMORIA DINAMICA , RESERVANDO Y LIBERANDO MEMORIA ************/
+//********ACLARACION , SE DEBERA MANEJAR CON LISTA DOBLEMENTE ENTRELAZADA , RESERVANDO Y LIBERANDO MEMORIA ************/
 
 // Estructura de datos que guarda la información
 
@@ -16,7 +16,7 @@ typedef struct
 
 typedef struct Nodo
 {
-    int id;             // año
+    int id;             // año del dataset
     char provincia[20]; // provincias
     char tipo[10];      // publico / privado
     registroMatricula reg;
@@ -36,7 +36,7 @@ typedef struct
 // Nodo de la lista doblemente enlazada
 typedef struct Nodo
 {
-    int id;             // año
+    int id;             // año del dataset
     char provincia[20]; // provincia
     char tipo[10];      // publico / privado
 
@@ -47,7 +47,7 @@ typedef struct Nodo
 
 //--------->Objetivo de funciones<--------
 
-//Funcion menu de opciones
+//-------->Funcion menu de opciones
 
 //******Para ambos nodos*******
 //--------->Mostrar nodo(debe mostrar la lista)
@@ -59,7 +59,105 @@ typedef struct Nodo
 //--------->Buscador por año(ofrecer la opcion de combinar con mas filtros)
 //--------->Buscador por tipo(ofrecer la opcion de combinar con mas filtros)
 
-//###################FUNCION LIBERAR MEMORIA###################//
+// ###################FUNCION LIBERAR MEMORIA###################//
 
+// prototipo de funciones
+
+void menu(Nodo *Matricula, Nodo *Trayectoria);
+// Esta funcion tiene que mostrar por consola una lista de opciones a, b, c, etc. Con el siguiente formato:
+// ------- MENU -------
+// a. Mostrar nodo
+// b. Mostrar datos de todas las provincias
+// c. Mostrar datos de todos los anios(2019, 2020, 2021)
+// d. Mostrar datos de educacion publica y privada
+// e. Buscar por provincia
+// f. Buscar por anio
+// g. Buscar por tipo
+// s. Salir
+// Tener en cuenta que al mostrar las ultimas tres opciones, hay que ofrecer si se quiere combinar el filtro que se elija con algun otro.
+
+void mostrarNodo(Nodo *Matricula, Nodo *Trayectoria);
+// En esta funcion se tiene que mostrar por consola en formato de tabla el conjunto de nodos con sus datos, haciendo una columna para matricula y otra columna para trayectoria. Ej:
+//                      Matricula                                       Trayectoria
+//                    Registro Num.1                                   Registro Num.1
+//                        Año                                               Año
+//                      Provincia                                        Provincia
+//                        Tipo                                              Tipo
+//                   Año de escolaridad                              Año de escolaridad
+//                       Matricula                                     Cant. de egresos
+//                       Repitentes                                       Sobre Edad
+
+void verTodasLasProvincias(Nodo *Matricula, Nodo *Trayectoria);
+// Esta funcion deberia depender de la provincia , lo que se deberia lograr es recorrer todos los datos que tenga guardado el nodo por cada provincia , es decir, ej: cordoba mostraria todos los registros guardados de matricula y trayectoria de esa provincia ademas deberia mostrarse por consola como 2 tablas para discriminar cuando es matricula y cuando es trayectoria por cada año escolar.
+// una validacion seria ir mostrando mismo año escolar y misma provincia y en caso de que algun nodo no tenga misma info que el otro mostrar en consola , contemplar el caso que alguno de los 2 puede no tener informacion de la provincia analizada o puede tener mas que el otro , en esos casos colocar un cartel que diga "no se encontraron registros" .
+// ********EJEMPLO consola*********
+//          matriculas cordoba                                              trayectorias cordoba
+//.    Registro matricula numero 1                                         Registro trayectoria numero 1
+//. año de escolaridad = 1 matriculas = 22 repitentes = 3        año de escolaridad = 1 secundariaEgresados = 22 sobreEdad = 5
+//
+
+void verTodosLosAnios(Nodo *Matricula, Nodo *Trayectoria);
+// Esta funcion deberia depender de la año , lo que se deberia lograr es recorrer todos los datos que tenga guardado el nodo por cada año , es decir, ej: 2020 mostraria todos los registros guardados de matricula y trayectoria de esa año ademas deberia mostrarse por consola como 2 tablas para discriminar cuando es matricula y cuando es trayectoria por cada año escolar.
+// una validacion seria ir mostrando mismo año escolar y misma año y en caso de que algun nodo no tenga misma info que el otro mostrar en consola , contemplar el caso que alguno de los 2 puede no tener informacion de la año analizada o puede tener mas que el otro , en esos casos colocar un cartel que diga "no se encontraron registros" .
+// ********EJEMPLO consola*********
+//                  matriculas 2020                                              trayectorias 2020
+//.           Registro matricula numero 1                                         Registro trayectoria numero 1
+//.         año de escolaridad = 1 matriculas = 22 repitentes = 3        año de escolaridad = 1 secundariaEgresados = 22 sobreEdad = 5
+//
+
+void verTodosLosTipos(Nodo *Matricula, Nodo *Trayectoria);
+// Esta funcion deberia depender del tipo(estatal/privado) , lo que se deberia lograr es recorrer todos los datos que tenga guardado el nodo por cada tipo , es decir, ej: puclico mostraria todos los registros guardados de matricula y trayectoria de esa tipo ademas deberia mostrarse por consola como 2 tablas para discriminar cuando es matricula y cuando es trayectoria por cada año escolar.
+// una validacion seria ir mostrando mismo año escolar y misma tipo y en caso de que algun nodo no tenga misma info que el otro mostrar en consola , contemplar el caso que alguno de los 2 puede no tener informacion de la tipo analizada o puede tener mas que el otro , en esos casos colocar un cartel que diga "no se encontraron registros" .
+// ********EJEMPLO consola*********
+//               matriculas puclico                                              trayectorias puclico
+//.        Registro matricula numero 1                                         Registro trayectoria numero 1
+//.         año de escolaridad = 1 matriculas = 22 repitentes = 3        año de escolaridad = 1 secundariaEgresados = 22 sobreEdad = 5
+
+
+
+
+
+//*********************ACLARACION SOBRE LAS FUNCIONES DE BUSQUEDA ***********************
+// la idea de poder combinar los filtros es que al tener 3 funciones de busqueda se deberia permitir al usuario combinar mas de una busqueda si asi lo desea, un ejemplo de uso seria , buscar por provincia , utilizar  nodos temporales(para matricula y trayectoria) para guardar el tipo de provincia que escribio el usuario , preguntar si desea filtrar por año o tipo , en caso de elegir alguna de estas opciones se deberia llamar a la funcion respectiva y pasar como parametro los nodos temporales ya que van a tener la informacion ya filtrada por provincia , filtrar de nuevo esa informacion por el caso que se solicita y mostrar o permitir combinar una vez mas , en caso de que no quiera realizar ninguna combinacion solo se muestra.
+//*****************************************************************************************/
+
+void buscarPorProvincia(Nodo *Matricula, Nodo *Trayectoria);
+// En esta funcion se tiene que pedir que se ingrese el nombre de la provincia cuyos datos se quiere ver, antes de empezar a mostrarlos, preguntar si se quiere añadir algun otro filtro. Por ej: Cordoba , una vez que ya filtramos los datos segun lo que se nos pide, mostramos la info por consola en un formato de tabla similar al de la funcion mostrarNodo.
+// Hacemos dos nodos temporales, uno para Matricula, y otro para Trayectoria, guardando los datos filtrados en caso de que se pida combinar con otro filtro, asi podemos pasarlos como parametros al llamar a otra funcion para hacer el siguiente filtro
+//                      Matricula                                       Trayectoria
+//                    Registro Num.1                                   Registro Num.1
+//                        Año                                               Año
+//                      Provincia                                        Provincia
+//                        Tipo                                              Tipo
+//                   Año de escolaridad                              Año de escolaridad
+//                       Matricula                                     Cant. de egresos
+//                       Repitentes                                       Sobre Edad
+
+void buscarPorAnio(Nodo *Matricula, Nodo *Trayectoria);
+// En esta funcion se tiene que pedir que se ingrese el año cuyos datos se quiere ver, antes de empezar a mostrarlos, preguntar si se quiere añadir algun otro filtro. Por ej: 2020 , una vez que ya filtramos los datos segun lo que se nos pide, mostramos la info por consola en un formato de tabla similar al de la funcion mostrarNodo.
+// Hacemos dos nodos temporales, uno para Matricula, y otro para Trayectoria, guardando los datos filtrados en caso de que se pida combinar con otro filtro, asi podemos pasarlos como parametros al llamar a otra funcion para hacer el siguiente filtro
+//                      Matricula                                       Trayectoria
+//                    Registro Num.1                                   Registro Num.1
+//                        Año                                               Año
+//                      Provincia                                        Provincia
+//                        Tipo                                              Tipo
+//                   Año de escolaridad                              Año de escolaridad
+//                       Matricula                                     Cant. de egresos
+//                       Repitentes                                       Sobre Edad
+
+void buscarPorTipo(Nodo *Matricula, Nodo *Trayectoria);
+// En esta funcion se tiene que pedir que se ingrese el tipo cuyos datos se quiere ver, antes de empezar a mostrarlos, preguntar si se quiere añadir algun otro filtro. Por ej: publico, una vez que ya filtramos los datos segun lo que se nos pide, mostramos la info por consola en un formato de tabla similar al de la funcion mostrarNodo.
+// Hacemos dos nodos temporales, uno para Matricula, y otro para Trayectoria, guardando los datos filtrados en caso de que se pida combinar con otro filtro, asi podemos pasarlos como parametros al llamar a otra funcion para hacer el siguiente filtro
+//                      Matricula                                       Trayectoria
+//                    Registro Num.1                                   Registro Num.1
+//                        Año                                               Año
+//                      Provincia                                        Provincia
+//                        Tipo                                              Tipo
+//                   Año de escolaridad                              Año de escolaridad
+//                       Matricula                                     Cant. de egresos
+//                       Repitentes                                       Sobre Edad
+
+void liberarMemoria(Nodo *Matricula, Nodo *Trayectoria);
+// esta funcion debera recorrer al nodo con la condicion que mientras encuentre otro nodo no frena el bucle, luego por cada bucle libera memoria asegurandose de que esten todos los nodos liberados(se utiliza un puntero nodo temporal)
 
 #endif
