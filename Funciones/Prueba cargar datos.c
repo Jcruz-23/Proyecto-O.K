ba11526447e61ee36f4619c16373d20b826f25eb
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2025-10-08 22:56:53 -0300

    Carga e impresion correcta de los datos trayectoria en su totalidad, impresion extra en .txt para ver todos los datos

--- Funciones/Prueba cargar datos.c
+++ Funciones/Prueba cargar datos.c
@@ -30,12 +30,12 @@ cantidad de egresados en la segundaria y cantidad de egresados de primaria;
 } Matricula;*/
 
 typedef struct {
-  int anio[6];                // años de escolaridad.
-  int secundariaEgresados[6]; // cantidad de egresos.
-  int promovidos[6];          // aprobados
-  int nopromovidos[6];        // no aprobados
-  char provincia[25];         // provincia del dato
-  char sector[15];            // estatal o privado
+  int anio[6];             // años de escolaridad.
+  int secundariaEgresados; // cantidad de egresos.
+  int promovidos[6];       // aprobados
+  int nopromovidos[6];     // no aprobados
+  char provincia[25];      // provincia del dato
+  char sector[15];         // estatal o privado
 } Trayectoria;
 
 /*typedef struct Nodo {
@@ -145,11 +145,11 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro) {
                 nuevo->trayectoria.nopromovidos[i] = atoi(dato);
               }
             }
-            for (int i = 0; i < 6; i++) {
-              if ((dato = strtok(NULL, ",")) != NULL) {
-                nuevo->trayectoria.secundariaEgresados[i] = atoi(dato);
-              }
+
+            if ((dato = strtok(NULL, ",")) != NULL) {
+              nuevo->trayectoria.secundariaEgresados = atoi(dato);
             }
+
             nuevo->id = 2019;
             for (int i = 0; i < 6; i++) {
               nuevo->trayectoria.anio[i] = i + 1;
@@ -202,11 +202,10 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro) {
                 nuevo->trayectoria.nopromovidos[i] = atoi(dato);
               }
             }
-            for (int i = 0; i < 6; i++) {
-              if ((dato = strtok(NULL, ",")) != NULL) {
-                nuevo->trayectoria.secundariaEgresados[i] = atoi(dato);
-              }
+            if ((dato = strtok(NULL, ",")) != NULL) {
+              nuevo->trayectoria.secundariaEgresados = atoi(dato);
             }
+
             nuevo->id = 2020;
             for (int i = 0; i < 6; i++) {
               nuevo->trayectoria.anio[i] = i + 1;
@@ -259,10 +258,8 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro) {
                 nuevo->trayectoria.nopromovidos[i] = atoi(dato);
               }
             }
-            for (int i = 0; i < 6; i++) {
-              if ((dato = strtok(NULL, ",")) != NULL) {
-                nuevo->trayectoria.secundariaEgresados[i] = atoi(dato);
-              }
+            if ((dato = strtok(NULL, ",")) != NULL) {
+              nuevo->trayectoria.secundariaEgresados = atoi(dato);
             }
             nuevo->id = 2021;
             for (int i = 0; i < 6; i++) {
@@ -293,183 +290,162 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro) {
 
 void ImprimirLista(NodoTrayectoria *registro) {
   NodoTrayectoria *aux = registro;
-  printf(
-      "%-25s | %-25s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | "
-      "%-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %s\n",
-      "Provincia", "Sector", "1_P", "2_P", "3_P", "4_P", "5_P", "6_P", "1_NP",
-      "2_NP", "3_NP", "4_NP", "5_NP", "6_NP", "1_E", "2_E", "3_E", "4_E", "5_E",
-      "6_E", "Anio");
+  printf("%-25s | %-25s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | "
+         "%-4s | %-4s | %-4s | %-4s | %-4s | %-9s | %s\n",
+         "Provincia", "Sector", "1_P", "2_P", "3_P", "4_P", "5_P", "6_P",
+         "1_NP", "2_NP", "3_NP", "4_NP", "5_NP", "6_NP", "Egresados", "Anio");
   while (aux != NULL) {
-    printf(
-        "%-25s | %-25s | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d "
-        "| %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %d\n",
-        aux->trayectoria.provincia, aux->trayectoria.sector,
-        aux->trayectoria.promovidos[0], aux->trayectoria.promovidos[1],
-        aux->trayectoria.promovidos[2], aux->trayectoria.promovidos[3],
-        aux->trayectoria.promovidos[4], aux->trayectoria.promovidos[5],
-        aux->trayectoria.nopromovidos[0], aux->trayectoria.nopromovidos[1],
-        aux->trayectoria.nopromovidos[2], aux->trayectoria.nopromovidos[3],
-        aux->trayectoria.nopromovidos[4], aux->trayectoria.nopromovidos[5],
-        aux->trayectoria.secundariaEgresados[0],
-        aux->trayectoria.secundariaEgresados[1],
-        aux->trayectoria.secundariaEgresados[2],
-        aux->trayectoria.secundariaEgresados[3],
-        aux->trayectoria.secundariaEgresados[4],
-        aux->trayectoria.secundariaEgresados[5], aux->id);
+    printf("%-25s | %-25s | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | "
+           "%-4d | %-4d | %-4d | %-4d | %-4d | %-9d | %d\n",
+           aux->trayectoria.provincia, aux->trayectoria.sector,
+           aux->trayectoria.promovidos[0], aux->trayectoria.promovidos[1],
+           aux->trayectoria.promovidos[2], aux->trayectoria.promovidos[3],
+           aux->trayectoria.promovidos[4], aux->trayectoria.promovidos[5],
+           aux->trayectoria.nopromovidos[0], aux->trayectoria.nopromovidos[1],
+           aux->trayectoria.nopromovidos[2], aux->trayectoria.nopromovidos[3],
+           aux->trayectoria.nopromovidos[4], aux->trayectoria.nopromovidos[5],
+           aux->trayectoria.secundariaEgresados, aux->id);
     aux = aux->sig;
-}
-    aux = registro;
+  }
+  aux = registro;
 
-    FILE *archivo = fopen("Registro Trayectoria.txt", "w");
-    if (archivo == NULL) {
-        printf("Error al abrir archivo de salida\n");
-        return;
-    }
+  FILE *archivo = fopen("Registro Trayectoria.txt", "w");
+  if (archivo == NULL) {
+    printf("Error al abrir archivo de salida\n");
+    return;
+  }
 
-    fprintf(archivo,
-        "%-25s | %-15s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | "
-        "%-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | "
-        "%-4s | %-4s | %-4s | %-4s | %s\n",
-        "Provincia", "Sector",
-        "1_P", "2_P", "3_P", "4_P", "5_P", "6_P",
-        "1_NP", "2_NP", "3_NP", "4_NP", "5_NP", "6_NP",
-        "1_E", "2_E", "3_E", "4_E", "5_E", "6_E",
-        "Año"
-    );
+  fprintf(archivo,
+          "%-25s | %-25s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | %-4s | "
+          "%-4s | %-4s | %-4s | %-4s | %-4s | %-9s | %s\n",
+          "Provincia", "Sector", "1_P", "2_P", "3_P", "4_P", "5_P", "6_P",
+          "1_NP", "2_NP", "3_NP", "4_NP", "5_NP", "6_NP", "Egresados", "Anio"
+        );
 
     while (aux != NULL) {
-        fprintf(archivo,
-            "%-25s | %-15s | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | "
-            "%-4d | %-4d | %-4d | %-4d | %-4d | %-4d | "
-            "%-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %d\n",
-            aux->trayectoria.provincia, aux->trayectoria.sector,
-            aux->trayectoria.promovidos[0], aux->trayectoria.promovidos[1],
-            aux->trayectoria.promovidos[2], aux->trayectoria.promovidos[3],
-            aux->trayectoria.promovidos[4], aux->trayectoria.promovidos[5],
-            aux->trayectoria.nopromovidos[0], aux->trayectoria.nopromovidos[1],
-            aux->trayectoria.nopromovidos[2], aux->trayectoria.nopromovidos[3],
-            aux->trayectoria.nopromovidos[4], aux->trayectoria.nopromovidos[5],
-            aux->trayectoria.secundariaEgresados[0], aux->trayectoria.secundariaEgresados[1],
-            aux->trayectoria.secundariaEgresados[2], aux->trayectoria.secundariaEgresados[3],
-            aux->trayectoria.secundariaEgresados[4], aux->trayectoria.secundariaEgresados[5],
-            aux->id
-        );
-        aux = aux->sig;
+      fprintf(
+          archivo,
+          "%-25s | %-25s | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | %-4d | "
+          "%-4d | %-4d | %-4d | %-4d | %-4d | %-9d | %d\n",
+          aux->trayectoria.provincia, aux->trayectoria.sector,
+          aux->trayectoria.promovidos[0], aux->trayectoria.promovidos[1],
+          aux->trayectoria.promovidos[2], aux->trayectoria.promovidos[3],
+          aux->trayectoria.promovidos[4], aux->trayectoria.promovidos[5],
+          aux->trayectoria.nopromovidos[0], aux->trayectoria.nopromovidos[1],
+          aux->trayectoria.nopromovidos[2], aux->trayectoria.nopromovidos[3],
+          aux->trayectoria.nopromovidos[4], aux->trayectoria.nopromovidos[5],
+          aux->trayectoria.secundariaEgresados, aux->id);
+      aux = aux->sig;
     }
-    fclose(archivo);
-    printf("Datos guardados correctamente\n");
+      fclose(archivo);
+      printf("Datos guardados correctamente\n");
   }
 
+    /*
+    void InsertarNodoMatricula(NodoMatricula **p) {
 
-/*
-void InsertarNodoMatricula(NodoMatricula **p) {
+      for (int i = 0; i < 3; i++) {
 
-  for (int i = 0; i < 3; i++) {
-
-    if (i == 0) {
-      NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
-      if (nuevo != NULL) {
-        FILE *archivo;
-        archivo = fopen("matricula2019.csv", "r");
-        if (archivo != NULL) {
-          fscanf(); // debe gusrdarse en nuevo.trayectoria;
-          /*Con fscanf y fprintf debemos leer los datos del archivo para
-          almacenarlos en cada mienbro de la estructura, el analisis deberia de
-          ser de la primera columna y unificar las provincias ya que en el
-          archivo estan repetidas porque lo hace por departamentos (si queremos
-          tambbien contar los alumnos por departamento no hace falta), ya que no
-          vamos a utilizar todas las columnas y las funciones fscanf y fprintf
-          estan hechas para leer lineas, hay que ver como analizar solo las que
-          queremos. una vez guardadas en un nodo pasaremos al siguiente donde
-          analizaremos el archivo de 2020 y luego al siguiente sonde
-          analizaremos el de 2021
-          fclose(archivo);
-        } else {
-          printf("Error al leer el archivo\n");
-          return;
+        if (i == 0) {
+          NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
+          if (nuevo != NULL) {
+            FILE *archivo;
+            archivo = fopen("matricula2019.csv", "r");
+            if (archivo != NULL) {
+              fscanf(); // debe gusrdarse en nuevo.trayectoria;
+              /*Con fscanf y fprintf debemos leer los datos del archivo para
+              almacenarlos en cada mienbro de la estructura, el analisis deberia
+    de ser de la primera columna y unificar las provincias ya que en el archivo
+    estan repetidas porque lo hace por departamentos (si queremos tambbien
+    contar los alumnos por departamento no hace falta), ya que no vamos a
+    utilizar todas las columnas y las funciones fscanf y fprintf estan hechas
+    para leer lineas, hay que ver como analizar solo las que queremos. una vez
+    guardadas en un nodo pasaremos al siguiente donde analizaremos el archivo de
+    2020 y luego al siguiente sonde analizaremos el de 2021 fclose(archivo); }
+    else { printf("Error al leer el archivo\n"); return;
+            }
+            nuevo->ant = NULL;
+            nuevo->sig = NULL;
+            *p = nuevo;
+          } else {
+            printf("Error al crear nuevo nodo\n");
+          }
         }
-        nuevo->ant = NULL;
-        nuevo->sig = NULL;
-        *p = nuevo;
-      } else {
-        printf("Error al crear nuevo nodo\n");
+        if (i == 1) {
+          NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
+          if (nuevo != NULL) {
+            FILE *archivo;
+            archivo = fopen("matricula2020.csv", "r");
+            if (archivo != NULL) {
+              fscanf(); // debe gusrdarse en nuevo.trayectoria;
+              /*Con fscanf y fprintf debemos leer los datos del archivo para
+              almacenarlos en cada mienbro de la estructura, el analisis deberia
+              de ser de la primera columna y unificar las provincias ya que en
+              el archivo estan repetidas porque lo hace por departamentos (si
+              queremos tambbien contar los alumnos por departamento no hace
+              falta), ya que no vamos a utilizar todas las columnas y las
+              funciones fscanf y fprintf estan hechas para leer lineas, hay que
+              ver como analizar solo las que queremos. una vez guardadas en un
+              nodo pasaremos al siguiente donde analizaremos el archivo de 2020
+              y luego al siguiente sonde analizaremos el de 2021
+              fclose(archivo);
+            } else {
+              printf("Error al leer el archivo\n");
+              return;
+            }
+            nuevo->sig = NULL;
+            NodoMatricula *aux = *p;
+            while (aux->sig != NULL) {
+              aux = aux->sig;
+            }
+            nuevo->ant = aux;
+            aux->sig = nuevo;
+          } else {
+            printf("Error al crear nuevo nodo\n");
+          }
+        }
+        if (i == 2) {
+          NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
+          if (nuevo != NULL) {
+            FILE *archivo;
+            archivo = fopen("matricula2021.csv", "r");
+            if (archivo != NULL) {
+              fscanf(); // debe gusrdarse en nuevo.trayectoria;
+              /*Con fscanf y fprintf debemos leer los datos del archivo para
+              almacenarlos en cada mienbro de la estructura, el analisis deberia
+              de ser de la primera columna y unificar las provincias ya que en
+              el archivo estan repetidas porque lo hace por departamentos (si
+              queremos tambbien contar los alumnos por departamento no hace
+              falta), ya que no vamos a utilizar todas las columnas y las
+              funciones fscanf y fprintf estan hechas para leer lineas, hay que
+              ver como analizar solo las que queremos. una vez guardadas en un
+              nodo pasaremos al siguiente donde analizaremos el archivo de 2020
+              y luego al siguiente sonde analizaremos el de 2021
+              fclose(archivo);
+            } else {
+              printf("Error al leer el archivo\n");
+              return;
+            }
+            nuevo->sig = NULL;
+            NodoMatricula *aux = *p;
+            while (aux->sig != NULL) {
+              aux = aux->sig;
+            }
+            nuevo->ant = aux;
+            aux->sig = nuevo;
+          } else {
+            printf("Error al crear nuevo nodo\n");
+          }
+        }
       }
     }
-    if (i == 1) {
-      NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
-      if (nuevo != NULL) {
-        FILE *archivo;
-        archivo = fopen("matricula2020.csv", "r");
-        if (archivo != NULL) {
-          fscanf(); // debe gusrdarse en nuevo.trayectoria;
-          /*Con fscanf y fprintf debemos leer los datos del archivo para
-          almacenarlos en cada mienbro de la estructura, el analisis deberia
-          de ser de la primera columna y unificar las provincias ya que en
-          el archivo estan repetidas porque lo hace por departamentos (si
-          queremos tambbien contar los alumnos por departamento no hace
-          falta), ya que no vamos a utilizar todas las columnas y las
-          funciones fscanf y fprintf estan hechas para leer lineas, hay que
-          ver como analizar solo las que queremos. una vez guardadas en un
-          nodo pasaremos al siguiente donde analizaremos el archivo de 2020
-          y luego al siguiente sonde analizaremos el de 2021
-          fclose(archivo);
-        } else {
-          printf("Error al leer el archivo\n");
-          return;
-        }
-        nuevo->sig = NULL;
-        NodoMatricula *aux = *p;
-        while (aux->sig != NULL) {
-          aux = aux->sig;
-        }
-        nuevo->ant = aux;
-        aux->sig = nuevo;
-      } else {
-        printf("Error al crear nuevo nodo\n");
-      }
-    }
-    if (i == 2) {
-      NodoMatricula *nuevo = (NodoMatricula *)malloc(sizeof(NodoMatricula));
-      if (nuevo != NULL) {
-        FILE *archivo;
-        archivo = fopen("matricula2021.csv", "r");
-        if (archivo != NULL) {
-          fscanf(); // debe gusrdarse en nuevo.trayectoria;
-          /*Con fscanf y fprintf debemos leer los datos del archivo para
-          almacenarlos en cada mienbro de la estructura, el analisis deberia
-          de ser de la primera columna y unificar las provincias ya que en
-          el archivo estan repetidas porque lo hace por departamentos (si
-          queremos tambbien contar los alumnos por departamento no hace
-          falta), ya que no vamos a utilizar todas las columnas y las
-          funciones fscanf y fprintf estan hechas para leer lineas, hay que
-          ver como analizar solo las que queremos. una vez guardadas en un
-          nodo pasaremos al siguiente donde analizaremos el archivo de 2020
-          y luego al siguiente sonde analizaremos el de 2021
-          fclose(archivo);
-        } else {
-          printf("Error al leer el archivo\n");
-          return;
-        }
-        nuevo->sig = NULL;
-        NodoMatricula *aux = *p;
-        while (aux->sig != NULL) {
-          aux = aux->sig;
-        }
-        nuevo->ant = aux;
-        aux->sig = nuevo;
-      } else {
-        printf("Error al crear nuevo nodo\n");
-      }
-    }
-  }
-}
-*/
+    */
 
-void Liberar(NodoTrayectoria **registro) {
-  while (*registro != NULL) {
-    NodoTrayectoria *prox = (*registro)->sig;
-    free(*registro);
-    *registro = prox;
-  }
-  printf("\nLa lista se ha eliminado con exito\n");
-}
+    void Liberar(NodoTrayectoria * *registro) {
+      while (*registro != NULL) {
+        NodoTrayectoria *prox = (*registro)->sig;
+        free(*registro);
+        *registro = prox;
+      }
+      printf("\nLa lista se ha eliminado con exito\n");
+    }
