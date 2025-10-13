d0872464af4188cbed20a26cfdc4d5e3fc69fb5a
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2025-10-13 18:36:17 -0300

    Funcion de guardado y clasificado listas

--- Funciones/Prueba cargar datos.c
+++ Funciones/Prueba cargar datos.c
@@ -6,40 +6,38 @@ typedef struct {
   int secundariaEgresados; // cantidad de egresos.
   int promovidos[6];       // aprobados
   int nopromovidos[6];     // no aprobados
-  char provincia[25];      // provincia del dato
-  char sector[10];         // estatal o privado
 } Trayectoria;
 
 typedef struct NodoTrayectoria {
   Trayectoria trayectoria;
   char provincia[25]; // provincia del dato
   char sector[10];    // estatal o privado
-  int id;
+  int id;  
   struct NodoTrayectoria *sig;
   struct NodoTrayectoria *ant;
 } NodoTrayectoria;
 
-void InsertarNodoTrayectoria(NodoTrayectoria **, Trayectoria);
+void InsertarNodoTrayectoria(NodoTrayectoria **);
 void ImprimirLista(NodoTrayectoria *);
 void Liberar(NodoTrayectoria **);
-int Lectura(Trayectoria *, FILE *);
-void Carga(NodoTrayectoria **, Trayectoria);
+int Lectura(Trayectoria *, FILE *, char [25], char [10]);
+void Carga(NodoTrayectoria **, Trayectoria, char [25], char [10]);
 void Clasificacion(NodoTrayectoria *, NodoTrayectoria **);
 
 int main() {
-  NodoTrayectoria *registro = NULL;
-  NodoTrayectoria *NuevoRegistro = NULL;
-  Trayectoria trayectoria = {0};
-  InsertarNodoTrayectoria(&registro, trayectoria);
-  Clasificacion(registro, &NuevoRegistro);
-  ImprimirLista(NuevoRegistro);
-  Liberar(&registro);
+  NodoTrayectoria *RegistroTrayectoria = NULL;
+  InsertarNodoTrayectoria(&RegistroTrayectoria);
+  ImprimirLista(RegistroTrayectoria);
+  Liberar(&RegistroTrayectoria);
   return 0;
 }
 
-void InsertarNodoTrayectoria(NodoTrayectoria **registro,
-                             Trayectoria trayectoria) {
-
+void InsertarNodoTrayectoria(NodoTrayectoria **RegistroTrayectoria) {
+  char provincia[25];
+  char sector[10];
+  NodoTrayectoria *temp = NULL;
+  Trayectoria trayectoria = {0};
+ 
   for (int i = 0; i < 3; i++) {
 
     if (i == 0) {
@@ -51,22 +49,22 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro,
         printf("\nNo se pudo leer el archivo\n");
       } else {
         fgets(basura, 20000, archivo);
-        while ((Lectura(&trayectoria, archivo)) == 15) {
+        while ((Lectura(&trayectoria, archivo, provincia, sector)) == 15) {
           NodoTrayectoria *nuevo =
               (NodoTrayectoria *)malloc(sizeof(NodoTrayectoria));
           if (nuevo != NULL) {
-            Carga(&nuevo, trayectoria);
+            Carga(&nuevo, trayectoria, provincia, sector);
             nuevo->id = 2019;
-            if (*registro == NULL) {
-              *registro = nuevo;
-              nuevo->ant = NULL;
+            if (temp == NULL) {
+              temp = nuevo;    
             } else {
-              NodoTrayectoria *aux = *registro;
+              NodoTrayectoria *aux = temp;
               while (aux->sig != NULL) {
                 aux = aux->sig;
               }
               nuevo->ant = aux;
               aux->sig = nuevo;
+             
             }
           } else {
             printf("Error al crear nuevo nodo\n");
@@ -86,22 +84,22 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro,
         printf("\nNo se pudo leer el archivo\n");
       } else {
         fgets(basura, 20000, archivo);
-        while ((Lectura(&trayectoria, archivo)) == 15) {
+        while ((Lectura(&trayectoria, archivo, provincia, sector)) == 15) {
           NodoTrayectoria *nuevo =
               (NodoTrayectoria *)malloc(sizeof(NodoTrayectoria));
           if (nuevo != NULL) {
-            Carga(&nuevo, trayectoria);
+            Carga(&nuevo, trayectoria, provincia, sector);
             nuevo->id = 2020;
-            if (*registro == NULL) {
-              *registro = nuevo;
-              nuevo->ant = NULL;
+            if (temp == NULL) {
+              temp = nuevo;
             } else {
-              NodoTrayectoria *aux = *registro;
+              NodoTrayectoria *aux = temp;
               while (aux->sig != NULL) {
                 aux = aux->sig;
               }
               nuevo->ant = aux;
               aux->sig = nuevo;
+           
             }
           } else {
             printf("Error al crear nuevo nodo\n");
@@ -120,22 +118,22 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro,
         printf("\nNo se pudo leer el archivo\n");
       } else {
         fgets(basura, 20000, archivo);
-        while ((Lectura(&trayectoria, archivo)) == 15) {
+        while ((Lectura(&trayectoria, archivo, provincia, sector)) == 15) {
           NodoTrayectoria *nuevo =
               (NodoTrayectoria *)malloc(sizeof(NodoTrayectoria));
           if (nuevo != NULL) {
-            Carga(&nuevo, trayectoria);
+            Carga(&nuevo, trayectoria, provincia, sector);
             nuevo->id = 2021;
-            if (*registro == NULL) {
-              *registro = nuevo;
-              nuevo->ant = NULL;
+            if (temp == NULL) {
+              temp = nuevo;
             } else {
-              NodoTrayectoria *aux = *registro;
+              NodoTrayectoria *aux = temp;
               while (aux->sig != NULL) {
                 aux = aux->sig;
               }
               nuevo->ant = aux;
               aux->sig = nuevo;
+              
             }
           } else {
             printf("Error al crear nuevo nodo\n");
@@ -146,13 +144,15 @@ void InsertarNodoTrayectoria(NodoTrayectoria **registro,
       printf("\nArchivo leido con exito\n");
     }
   }
+
+  Clasificacion(temp, RegistroTrayectoria);
 }
 
-int Lectura(Trayectoria *trayectoria, FILE *archivo) {
+int Lectura(Trayectoria *trayectoria, FILE *archivo, char provincia[25], char sector[10]) {
   int cant = 0;
   cant = (fscanf(
       archivo, "%25[^,],%10[^,],%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",
-      trayectoria->provincia, trayectoria->sector, &trayectoria->promovidos[0],
+      provincia, sector, &trayectoria->promovidos[0],
       &trayectoria->promovidos[1], &trayectoria->promovidos[2],
       &trayectoria->promovidos[3], &trayectoria->promovidos[4],
       &trayectoria->promovidos[5], &trayectoria->nopromovidos[0],
@@ -162,13 +162,12 @@ int Lectura(Trayectoria *trayectoria, FILE *archivo) {
   return cant;
 }
 
-void Carga(NodoTrayectoria **nuevo, Trayectoria trayectoria) {
-  (*nuevo)->sig = NULL;
+void Carga(NodoTrayectoria **nuevo, Trayectoria trayectoria, char provincia[25], char sector[10]) {
+  
   (*nuevo)->ant = NULL;
-  strcpy((*nuevo)->trayectoria.provincia, trayectoria.provincia);
-  strcpy((*nuevo)->trayectoria.sector, trayectoria.sector);
-  strcpy((*nuevo)->provincia, trayectoria.provincia);
-  strcpy((*nuevo)->sector, trayectoria.sector);
+  (*nuevo)->sig = NULL;
+  strcpy((*nuevo)->provincia, provincia);
+  strcpy((*nuevo)->sector, sector);
   for (int i = 0; i < 6; i++) {
     (*nuevo)->trayectoria.promovidos[i] = trayectoria.promovidos[i];
     (*nuevo)->trayectoria.nopromovidos[i] = trayectoria.nopromovidos[i];
@@ -176,18 +175,18 @@ void Carga(NodoTrayectoria **nuevo, Trayectoria trayectoria) {
   (*nuevo)->trayectoria.secundariaEgresados = trayectoria.secundariaEgresados;
 }
 
-void Clasificacion(NodoTrayectoria *registro, NodoTrayectoria **NuevoRegistro) {
-  NodoTrayectoria *temp = registro;
+void Clasificacion(NodoTrayectoria *temp, NodoTrayectoria **RegistroTrayectoria) {
+  NodoTrayectoria *tempo = temp;
 
-  while (temp != NULL) {
+  while (tempo != NULL) {
 
     int bandera = 0;
-    NodoTrayectoria *aux = *NuevoRegistro;
+    NodoTrayectoria *aux = *RegistroTrayectoria;
 
     while (aux != NULL && bandera == 0) {
-      if (strcmp(temp->provincia, aux->provincia) == 0 &&
-          strcmp(temp->sector, aux->sector) == 0 &&
-          temp->id == aux->id) { // Buscar nodo en la lista nueva primero
+      if (strcmp(tempo->provincia, aux->provincia) == 0 &&
+          strcmp(tempo->sector, aux->sector) == 0 &&
+          tempo->id == aux->id) { // Buscar nodo en la lista nueva primero
         bandera = 1;
       } else {
         aux = aux->sig; // Avanza hasta encontrar el nodo con las coincidencias
@@ -195,11 +194,11 @@ void Clasificacion(NodoTrayectoria *registro, NodoTrayectoria **NuevoRegistro) {
     }
     if (bandera) { // Si encuentra coincidencias las suma a nuestro nodo
       for (int i = 0; i < 6; i++) {
-        aux->trayectoria.nopromovidos[i] += temp->trayectoria.nopromovidos[i];
-        aux->trayectoria.promovidos[i] += temp->trayectoria.promovidos[i];
+        aux->trayectoria.nopromovidos[i] += tempo->trayectoria.nopromovidos[i];
+        aux->trayectoria.promovidos[i] += tempo->trayectoria.promovidos[i];
       }
       aux->trayectoria.secundariaEgresados +=
-          temp->trayectoria.secundariaEgresados;
+          tempo->trayectoria.secundariaEgresados;
 
     } else { // Si no las encuentra crea un nuevo nodo y lo agrega a la lista
              // nueva
@@ -208,44 +207,41 @@ void Clasificacion(NodoTrayectoria *registro, NodoTrayectoria **NuevoRegistro) {
       if (nuevo != NULL) {
         nuevo->ant = NULL;
         nuevo->sig = NULL;
-        strcpy(nuevo->provincia, temp->provincia);
-        strcpy(nuevo->sector, temp->sector);
-        strcpy(nuevo->trayectoria.provincia, temp->trayectoria.provincia);
-        strcpy(nuevo->trayectoria.sector, temp->trayectoria.sector);
-        nuevo->id = temp->id;
+        strcpy(nuevo->provincia, tempo->provincia);
+        strcpy(nuevo->sector, tempo->sector);
+        nuevo->id = tempo->id;
 
         for (int i = 0; i < 6; i++) {
           nuevo->trayectoria.nopromovidos[i] =
-              temp->trayectoria.nopromovidos[i];
-          nuevo->trayectoria.promovidos[i] = temp->trayectoria.promovidos[i];
+              tempo->trayectoria.nopromovidos[i];
+          nuevo->trayectoria.promovidos[i] = tempo->trayectoria.promovidos[i];
         }
         nuevo->trayectoria.secundariaEgresados =
-            temp->trayectoria.secundariaEgresados;
+            tempo->trayectoria.secundariaEgresados;
         // Agregando a la lista nueva
-        if (*NuevoRegistro == NULL) {
-          *NuevoRegistro = nuevo;
-          nuevo->ant = NULL;
+        if (*RegistroTrayectoria == NULL) {
+          *RegistroTrayectoria = nuevo;
         } else {
-          NodoTrayectoria *aux2 = *NuevoRegistro;
+          NodoTrayectoria *aux2 = *RegistroTrayectoria;
           while (aux2->sig != NULL) {
             aux2 = aux2->sig;
           }
           nuevo->ant = aux2;
           aux2->sig = nuevo;
-          nuevo->sig = NULL;
         }
       } else {
         printf("\nNo se ha podido crear el nodo\n");
       }
     }
-    temp = temp->sig;
+    tempo = tempo->sig;
   }
+  Liberar(&temp);
 }
 
-void ImprimirLista(NodoTrayectoria *registro) {
-  NodoTrayectoria *aux = registro;
-  printf("%-25s | %-10s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | "
-         "%-7s | %-7s | %-7s | %-7s | %-7s | %-9s | %s\n",
+void ImprimirLista(NodoTrayectoria *temp) {
+  NodoTrayectoria *aux = temp;
+  printf("%-25s | %-10s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | "
+         "%-6s | %-6s | %-6s | %-6s | %-6s | %-9s | %s\n",
          "Provincia", "Sector", "1_P", "2_P", "3_P", "4_P", "5_P", "6_P",
          "1_NP", "2_NP", "3_NP", "4_NP", "5_NP", "6_NP", "Egresados", "Anio");
   while (aux != NULL) {
@@ -261,17 +257,17 @@ void ImprimirLista(NodoTrayectoria *registro) {
            aux->trayectoria.secundariaEgresados, aux->id);
     aux = aux->sig;
   }
-  aux = registro;
+  aux = temp;
 
-  FILE *archivo = fopen("Registro Trayectoria.txt", "w");
+  FILE *archivo = fopen("temp Trayectoria.txt", "w");
   if (archivo == NULL) {
     printf("Error al abrir archivo de salida\n");
     return;
   }
 
   fprintf(archivo,
-          "%-25s | %-10s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | "
-          "%-7s | %-7s | %-7s | %-7s | %-7s | %-9s | %s\n",
+          "%-25s | %-10s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | "
+          "%-6s | %-6s | %-6s | %-6s | %-6s | %-9s | %s\n",
           "Provincia", "Sector", "1_P", "2_P", "3_P", "4_P", "5_P", "6_P",
           "1_NP", "2_NP", "3_NP", "4_NP", "5_NP", "6_NP", "Egresados", "Anio");
 
@@ -293,11 +289,11 @@ void ImprimirLista(NodoTrayectoria *registro) {
   printf("Datos guardados correctamente\n");
 }
 
-void Liberar(NodoTrayectoria **registro) {
-  while (*registro != NULL) {
-    NodoTrayectoria *prox = (*registro)->sig;
-    free(*registro);
-    *registro = prox;
+void Liberar(NodoTrayectoria **temp) {
+  while (*temp != NULL) {
+    NodoTrayectoria *prox = (*temp)->sig;
+    free(*temp);
+    *temp = prox;
   }
   printf("\nLa lista se ha eliminado con exito\n");
 }
